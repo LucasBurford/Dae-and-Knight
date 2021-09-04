cb553020c8e8079f0f8f9a758b64cbd397a284c2
Lucas-Burford <lucasburford@live.co.uk>
2021-09-04 16:08:08 +0100

    Set up project

---
+++ Game/Dae and Knight/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    // Store reference to GameManager
+    private GameManager gameManager;
+
+    // Store reference to CharacterController
+    public CharacterController2D controller;
+
+    // Store movement speed
+    private float moveSpeed;
+
+    private float horizontalMove = 0f;
+
+    // Store reference to Knight
+    [SerializeField]
+    private GameObject knight;
+
+    // Store reference to Dae
+    [SerializeField]
+    private GameObject dae;
+
+    /// <summary>
+    /// Get and set moveSpeed
+    /// </summary>
+    public float MoveSpeed
+    {
+        get { return moveSpeed; }
+        set { moveSpeed = value; }
+    }
+
+    /// <summary>
+    /// Start is called before the first frame
+    /// </summary>
+    void Start()
+    {
+        // Intantiate gameManager
+        gameManager = FindObjectOfType<GameManager>();
+
+        // Initialise moveSpeed
+        moveSpeed = 0.3f;
+    }
+
+    /// <summary>
+    /// Update is called every frame
+    /// </summary>
+    void Update()
+    {
+        KeyboardInput();
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    /// <summary>
+    /// Handle keyboard input
+    /// </summary>
+    private void KeyboardInput()
+    {
+        // If W is pressed
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // If state is day
+            if (gameManager.CheckState() == GameManager.GameState.Day)
+            {
+                // Move Dae up
+                dae.transform.position = new Vector2(dae.transform.position.x, dae.transform.position.y + moveSpeed);
+            }
+
+            // If state is night
+            if (gameManager.CheckState() == GameManager.GameState.Night)
+            {
+                // Move Knight up
+                knight.transform.position = new Vector2(knight.transform.position.x, knight.transform.position.y + moveSpeed);
+            }
+        }
+
+        // If A is pressed
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // If state is day
+            if (gameManager.CheckState() == GameManager.GameState.Day)
+            {
+                // Move Dae to the left
+                dae.transform.position = new Vector2(dae.transform.position.x - moveSpeed, dae.transform.position.y);
+            }
+
+            // If state is night
+            if (gameManager.CheckState() == GameManager.GameState.Night)
+            {
+                // Move Knight to the left
+                knight.transform.position = new Vector2(knight.transform.position.x - moveSpeed, knight.transform.position.y);
+            }
+        }
+
+        // If S is pressed
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // If state is day
+            if (gameManager.CheckState() == GameManager.GameState.Day)
+            {
+                // Move Dae down
+                dae.transform.position = new Vector2(dae.transform.position.x, dae.transform.position.y - moveSpeed);
+            }
+
+            if (gameManager.CheckState() == GameManager.GameState.Night)
+            {
+                // Move Knight down
+                knight.transform.position = new Vector2(knight.transform.position.x, knight.transform.position.y - moveSpeed);
+            }
+        }
+
+        // If D is pressed
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // If state is day
+            if (gameManager.CheckState() == GameManager.GameState.Day)
+            {
+                // Move Dae right
+                dae.transform.position = new Vector2(dae.transform.position.x + moveSpeed, dae.transform.position.y);
+            }
+
+            // If state is night
+            if (gameManager.CheckState() == GameManager.GameState.Night)
+            {
+                // Move Knight right
+                knight.transform.position = new Vector2(knight.transform.position.x + moveSpeed, knight.transform.position.y);
+            }
+        }
+
+        // If E is pressed
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Change state to day
+            gameManager.ChangeState(KeyCode.E);
+        }
+
+        // If Q is pressed
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // Change state to night
+            gameManager.ChangeState(KeyCode.Q);
+        }
+    }
+}
---
+++ Game/Dae and Knight/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Store reference to light
+    public Light light;
+
+    // Enum to store game state, night or day
+    public enum GameState
+    {
+        Day,
+        Night
+    }
+    // Access enum
+    public GameState gameState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialise the game state to night, as we start out as the knight
+        gameState = GameState.Night;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call CheckState to check states every frame
+        CheckState();
+    }
+
+    /// <summary>
+    /// Check gameState every frame and act accordingly
+    /// </summary>
+    public GameState CheckState()
+    {
+        // If state is day, keep light on
+        if (gameState == GameState.Day)
+        {
+            light.enabled = true;
+        }
+        else
+        {
+            light.enabled = false;
+        }
+
+        return gameState;
+    }
+
+    /// <summary>
+    /// Change the state of the game
+    /// </summary>
+    public void ChangeState(KeyCode key)
+    {
+        // If Q is passed through (pressed)
+        if (key == KeyCode.Q)
+        {
+            // Change the state to night
+            gameState = GameState.Night;
+        }
+
+        // If E is passed through (pressed)
+        if (key == KeyCode.E)
+        {
+            // Change the state to day
+            gameState = GameState.Day;
+        }
+    }
+}
